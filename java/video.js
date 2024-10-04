4beac3d3241857596491cb62e8f311bec8c97e76
mutasim99 <virginmr882@gmail.com>
2024-10-04 20:37:03 +0600

    added video section

--- java/video.js
+++ java/video.js
@@ -15,5 +15,54 @@ const displayCategories = (display) => {
     })
 }
 
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(err => console.log("Error:", err))
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('video-container')
+    videos.forEach(videos => {
+        console.log(videos);
+        const div = document.createElement('div')
+        div.classList = 'card card-compact'
+        div.innerHTML = `
+        <figure class="h-[200px] relative">
+    <img
+      src=${videos.thumbnail}
+      class="w-full h-full object-cover"
+      alt="Shoes" />
+      ${
+        videos.others.posted_date?.length === 0 ? '' : `<span class ="absolute bottom-2 right-3 bg-black text-white rounded-md p-1">
+        ${videos.others.posted_date}
+      </span>`
+      }
+      
+  </figure>
+    <div class="px-0 py-2 flex gap-2">
+    <div>
+        <img src=${videos.authors[0].profile_picture} class="w-10 h-10 rounded-full object-cover">
+    </div>
+    <div>
+        <h2 class="font-bold">${videos.title}</h2>
+        <div class="flex items-center gap-2">
+            <div>
+                <p>${videos.authors[0].profile_name}</p>
+            </div>
+            <div>
+                ${
+                    videos.authors[0]. verified === true ? '<img class="w-5 h-5 rounded-full object-cover" src=https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png>' : ''
+                }
+            </div>
+        </div>
+    </div>
+    </div>
+    `
+    videoContainer.append(div)
+    })
+}
 
 loadCatefories();
+loadVideos();
